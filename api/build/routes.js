e8fa404f41bf3b90f971802b950ea95c2a9e69aa
Gustavo Gomes Andrade <gustavo.g.a@icloud.com>
2021-10-15 21:22:56 -0300

    project init
    Primeiras configurações da Rest API. Testando o ambiente com typecript

---
+++ api/build/routes.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.router = void 0;
+var express_1 = require("express");
+var router = (0, express_1.Router)();
+exports.router = router;
+router.post('/users', function (request, response) {
+    return response.status(201).send();
+});
